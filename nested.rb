96aefe9c0edf5c8b65e178d951fce97ca6261a9a
Levik Naparstek <levik010@gmail.com>
2019-02-22 19:50:13 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -87,7 +87,11 @@ def adding_matz
         }
      }
 
-    programmer_hash[:yukihiro_matsumoto][]
+    programmer_hash[:yukihiro_matsumoto] = {
+    known_for: "Ruby",
+    languages:
+      ["LISP", "C"]
+    }
 end
 
 def changing_alan
@@ -107,6 +111,7 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
+     programmer_hash[:alan_kay]
      alans_new_info = "GUI"
      
      
